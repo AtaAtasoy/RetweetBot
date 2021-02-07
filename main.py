d16521d5e7ef7078eb15bd40dc966d750d263e1c
Ata Atasoy <h.ataatasoy@gmail.com>
2021-02-07 14:02:18 +0300

    add datetime to print statements

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import tweepy
 import os
 from dotenv import load_dotenv
 import time
+from datetime import datetime
 
 load_dotenv()
 
@@ -15,15 +16,17 @@ auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
 api = tweepy.API(auth)
 
+now = datetime.now()
+
 while True:
     wanted_tweet = api.get_status(1357643189977505792)
     if (wanted_tweet.retweeted):
         api.unretweet(1357643189977505792)
-        print("Unretweeted")
-        time.sleep(10800)
+        print("Unretweeted at : ", now.strftime("%d/%m/%Y %H:%M:%S"))
+        time.sleep(7200)
     else:
         api.retweet(1357643189977505792)
-        print("Retweeted")
+        print("Retweeted at : ", now.strftime("%d/%m/%Y %H:%M:%S"))
         time.sleep(3600)
 
 
