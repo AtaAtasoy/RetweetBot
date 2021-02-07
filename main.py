e53ab1a3716989f46cae1a93042560d4fde6058c
Ata Atasoy <h.ataatasoy@gmail.com>
2021-02-07 19:33:31 +0300

    Fix datetime

--- main.py
+++ main.py
@@ -16,17 +16,17 @@ auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
 api = tweepy.API(auth)
 
-now = datetime.now()
 
 while True:
     wanted_tweet = api.get_status(1357643189977505792)
+
     if (wanted_tweet.retweeted):
         api.unretweet(1357643189977505792)
-        print("Unretweeted at : ", now.strftime("%d/%m/%Y %H:%M:%S"))
+        print("Unretweeted at : ", datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
         time.sleep(7200)
     else:
         api.retweet(1357643189977505792)
-        print("Retweeted at : ", now.strftime("%d/%m/%Y %H:%M:%S"))
+        print("Retweeted at : ", datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
         time.sleep(3600)
 
 
